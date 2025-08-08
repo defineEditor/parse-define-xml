b4c5493a5fa41b6571d6e24b0f4bea2a317edf1b
Dmitry <dmitry.m.kolosov@gmail.com>
2025-08-07 23:28:06 -0300

    Adding 2.0 parser and tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,17 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import airbnb from 'eslint-config-airbnb';
-import prettier from 'eslint-config-prettier';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-  airbnb,
-  prettier,
-);
+export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, prettier, {
+    rules: {
+        "no-restricted-imports": [
+            "error",
+            {
+                patterns: [".*"],
+            },
+        ],
+        "no-trailing-spaces": "error",
+        "func-style": ["error", "expression"],
+        indent: ["error", 4],
+    },
+});
--- src/interfaces/define.xml.20.d.ts
+++ src/interfaces/define.xml.20.d.ts
@@ -1,216 +1,234 @@
 // Define-XML 2.0 interfaces (attributes use lowerCamelCase comparing to the XML specification)
 // See also: main.d.ts for shared interfaces
 
-export type ItemGroupDefClass = 'SPECIAL PURPOSE' | 'FINDINGS' | 'EVENTS' | 'INTERVENTIONS' | 'TRIAL DESIGN' | 'RELATIONSHIP' | 'SUBJECT LEVEL ANALYSIS DATASET' | 'BASIC DATA STRUCTURE' | 'ADAM OTHER';
-export type ItemGroupDefPurpose = 'Tabulation' | 'Analysis';
-export type PdfPageRefType = 'PhysicalRef' | 'NamedDestination';
-export type OriginType = 'CRF' | 'Derived' | 'Assigned' | 'Protocol' | 'eDT' | 'Predecessor';
-export type Comparator = 'LT' | 'LE' | 'GT' | 'GE' | 'EQ' | 'NE' | 'IN' | 'NOTIN';
-export type SoftHard = 'Soft' | 'Hard';
-export type CodeListType = 'text' | 'float' | 'integer';
-export type ItemDefDataType = 'text' | 'float' | 'integer' | 'date' | 'datetime';
+export type ItemGroupDefClass =
+    | "SPECIAL PURPOSE"
+    | "FINDINGS"
+    | "EVENTS"
+    | "INTERVENTIONS"
+    | "TRIAL DESIGN"
+    | "RELATIONSHIP"
+    | "SUBJECT LEVEL ANALYSIS DATASET"
+    | "BASIC DATA STRUCTURE"
+    | "ADAM OTHER";
+export type ItemGroupDefPurpose = "Tabulation" | "Analysis";
+export type PdfPageRefType = "PhysicalRef" | "NamedDestination";
+export type OriginType = "CRF" | "Derived" | "Assigned" | "Protocol" | "eDT" | "Predecessor";
+export type Comparator = "LT" | "LE" | "GT" | "GE" | "EQ" | "NE" | "IN" | "NOTIN";
+export type SoftHard = "Soft" | "Hard";
+export type CodeListType = "text" | "float" | "integer";
+export type ItemDefDataType = "text" | "float" | "integer" | "date" | "datetime";
 
 export interface Alias {
-  context: string;
-  name: string;
+    context: string;
+    name: string;
 }
 
 export interface TranslatedText {
-  xml_lang?: string;
-  value: string;
+    xml_lang?: string;
+    value: string;
 }
 
 export interface Leaf {
-  id: string;
-  xlink_href: string;
-  title: string;
+    id: string;
+    xlink_href: string;
+    title: string;
 }
 
 export interface DocumentRef {
-  leafId: string;
-  pdfPageRefs?: PdfPageRef[];
+    leafId: string;
+    pdfPageRefs?: PdfPageRef[];
 }
 
 export interface PdfPageRef {
-  type: PdfPageRefType;
-  pageRefs?: string;
-  firstPage?: number;
-  lastPage?: number;
+    type: PdfPageRefType;
+    pageRefs?: string;
+    firstPage?: number;
+    lastPage?: number;
 }
 
 export interface Origin {
-  type: OriginType;
-  descriptions?: TranslatedText[];
-  documentRefs?: DocumentRef[];
+    type: OriginType;
+    descriptions?: TranslatedText[];
+    documentRefs?: DocumentRef[];
 }
 
 export interface WhereClauseRef {
-  whereClauseOid: string;
+    whereClauseOid: string;
 }
 
 export interface WhereClauseDef {
-  oid: string;
-  commentOid?: string;
-  rangeChecks: RangeCheck[];
+    oid: string;
+    commentOid?: string;
+    rangeChecks: RangeCheck[];
 }
 
 export interface RangeCheck {
-  comparator: Comparator;
-  softHard: SoftHard;
-  itemOid: string;
-  checkValues: string[];
+    comparator: Comparator;
+    softHard: SoftHard;
+    itemOid: string;
+    checkValues: string[];
 }
 
 export interface CodeList {
-  oid: string;
-  name: string;
-  dataType: CodeListType;
-  sasFormatName?: string;
-  alias?: Alias[];
-  // Children (one of the following groups is required)
-  enumeratedItems?: EnumeratedItem[];
-  codeListItems?: CodeListItem[];
-  externalCodeList?: ExternalCodeList;
+    oid: string;
+    name: string;
+    dataType: CodeListType;
+    sasFormatName?: string;
+    alias?: Alias[];
+    // Children (one of the following groups is required)
+    enumeratedItems?: EnumeratedItem[];
+    codeListItems?: CodeListItem[];
+    externalCodeList?: ExternalCodeList;
 }
 
 export interface ExternalCodeList {
-  dictionary: string;
-  version: string;
-  ref?: string;
-  href?: string;
+    dictionary: string;
+    version: string;
+    ref?: string;
+    href?: string;
 }
 
 export interface EnumeratedItem {
-  codedValue: string;
-  rank?: number;
-  orderNumber?: number;
-  extendedValue?: 'Yes';
-  alias?: Alias[];
+    codedValue: string;
+    rank?: number;
+    orderNumber?: number;
+    extendedValue?: "Yes";
+    alias?: Alias[];
 }
 
 export interface CodeListItem {
-  codedValue: string;
-  rank?: number;
-  orderNumber?: number;
-  extendedValue?: 'Yes';
-  decode: TranslatedText[];
-  alias?: Alias[];
+    codedValue: string;
+    rank?: number;
+    orderNumber?: number;
+    extendedValue?: "Yes";
+    decode: TranslatedText[];
+    alias?: Alias[];
 }
 
 export interface CommentDef {
-  oid: string;
-  descriptions?: TranslatedText[];
-  documentRefs?: DocumentRef[];
+    oid: string;
+    descriptions?: TranslatedText[];
+    documentRefs?: DocumentRef[];
 }
 
 export interface FormalExpression {
-  context: string;
-  value: string;
+    context: string;
+    value: string;
 }
 
 export interface MethodDef {
-  oid: string;
-  name: string;
-  type: 'Computation' | 'Imputation';
-  descriptions?: TranslatedText[];
-  documentRefs?: DocumentRef[];
-  formalExpressions?: FormalExpression[];
+    oid: string;
+    name: string;
+    type: "Computation" | "Imputation";
+    descriptions?: TranslatedText[];
+    documentRefs?: DocumentRef[];
+    formalExpressions?: FormalExpression[];
 }
 
 export interface MetaDataVersion {
-  oid: string;
-  name: string;
-  description?: string;
-  defineVersion: string;
-  standardName: string;
-  standardVersion: string;
-  annotatedCrf?: DocumentRef[];
-  supplementalDoc?: DocumentRef[];
-  valueListDefs?: Record<string, ValueListDef>;
-  whereClauseDefs?: Record<string, WhereClauseDef>;
-  itemGroupDefs?: Record<string, ItemGroupDef>;
-  itemDefs?: Record<string, ItemDef>;
-  codeLists?: Record<string, CodeList>;
-  methodDefs?: Record<string, MethodDef>;
-  commentDefs?: Record<string, CommentDef>;
-  leafs?: Record<string, Leaf>;
+    oid: string;
+    name: string;
+    description?: string;
+    defineVersion: string;
+    standardName: string;
+    standardVersion: string;
+    itemGroupDefs: Record<string, ItemGroupDef>;
+    itemGroupDefsOrder: string[];
+    itemDefs: Record<string, ItemDef>;
+    itemDefsOrder: string[];
+    annotatedCrf?: DocumentRef[];
+    supplementalDoc?: DocumentRef[];
+    valueListDefs?: Record<string, ValueListDef>;
+    valueListDefsOrder?: string[];
+    whereClauseDefs?: Record<string, WhereClauseDef>;
+    whereClauseDefsOrder?: string[];
+    codeLists?: Record<string, CodeList>;
+    codeListsOrder?: string[];
+    methodDefs?: Record<string, MethodDef>;
+    methodDefsOrder?: string[];
+    commentDefs?: Record<string, CommentDef>;
+    commentDefsOrder?: string[];
+    leafs?: Record<string, Leaf>;
+    leafsOrder?: string[];
 }
 
 export interface ValueListDef {
-  oid: string;
-  itemRefs: ItemRef[];
+    oid: string;
+    itemRefs: Record<string, ItemRef>;
+    itemRefsOrder: string[];
 }
 
 export interface GlobalVariables {
-  studyName: string;
-  studyDescription: string;
-  protocolName: string;
+    studyName: string;
+    studyDescription: string;
+    protocolName: string;
 }
 
 export interface Study {
-  studyOid: string;
-  globalVariables: GlobalVariables;
-  metaDataVersion: MetaDataVersion;
+    studyOid: string;
+    globalVariables: GlobalVariables;
+    metaDataVersion: MetaDataVersion;
 }
 
 export interface Odm {
-  xmlns: 'http://www.cdisc.org/ns/odm/v1.3';
-  xmlns_def: 'http://www.cdisc.org/ns/def/v2.0';
-  xmlns_xlink?: 'http://www.w3.org/1999/xlink'; // Conditional
-  xmlns_xsi?: 'http://www.w3.org/2001/XMLSchema-instance'; // Conditional
-  xsi_schemaLocation?: string; // Optional
-  odmVersion: '1.3.2';
-  fileType: 'Snapshot';
-  fileOid: string;
-  creationDateTime: string; // ISO8601 datetime
-  asOfDateTime?: string; // ISO8601 datetime
-  originator?: string;
-  sourceSystem?: string;
-  sourceSystemVersion?: string;
-  study: Study;
+    xmlns: "http://www.cdisc.org/ns/odm/v1.3";
+    xmlns_def: "http://www.cdisc.org/ns/def/v2.0";
+    xmlns_xlink?: "http://www.w3.org/1999/xlink"; // Conditional
+    xmlns_xsi?: "http://www.w3.org/2001/XMLSchema-instance"; // Conditional
+    xsi_schemaLocation?: string; // Optional
+    odmVersion: "1.3.2";
+    fileType: "Snapshot";
+    fileOid: string;
+    creationDateTime: string; // ISO8601 datetime
+    asOfDateTime?: string; // ISO8601 datetime
+    originator?: string;
+    sourceSystem?: string;
+    sourceSystemVersion?: string;
+    study: Study;
 }
 
 export interface ItemGroupDef {
-  oid: string;
-  name: string;
-  repeating: 'Yes' | 'No';
-  purpose: ItemGroupDefPurpose;
-  domain?: string; // Required for SDTM and SEND
-  sasDatasetName?: string; // Required for regulatory submissions
-  structure?: string; // Required for regulatory submissions
-  class?: ItemGroupDefClass; // Required for regulatory submissions
-  archiveLocationId?: string; // Required for regulatory submissions
-  isReferenceData?: 'Yes' | 'No';
-  commentOid?: string;
-  description?: TranslatedText[];
-  itemRefs: ItemRef[];
-  alias?: Alias[];
-  leaf?: Leaf;
+    oid: string;
+    name: string;
+    repeating: "Yes" | "No";
+    purpose: ItemGroupDefPurpose;
+    domain?: string; // Required for SDTM and SEND
+    sasDatasetName?: string; // Required for regulatory submissions
+    structure?: string; // Required for regulatory submissions
+    class?: ItemGroupDefClass; // Required for regulatory submissions
+    archiveLocationId?: string; // Required for regulatory submissions
+    isReferenceData?: "Yes" | "No";
+    commentOid?: string;
+    description?: TranslatedText[];
+    itemRefs: Record<string, ItemRef>;
+    itemRefsOrder?: string[];
+    alias?: Alias[];
+    leaf?: Leaf;
 }
 
-
 export interface ItemDef {
-  oid: string;
-  name: string;
-  dataType: ItemDefDataType;
-  length?: number; // Required if DataType is "text", "integer", or "float"
-  significantDigits?: number; // Required if DataType is "float"
-  sasFieldName?: string; // Required for regulatory submissions
-  displayFormat?: string;
-  commentOid?: string;
-  description?: TranslatedText[];
-  codeListRef?: string;
-  origin?: Origin;
-  valueListRef?: string;
+    oid: string;
+    name: string;
+    dataType: ItemDefDataType;
+    length?: number; // Required if DataType is "text", "integer", or "float"
+    significantDigits?: number; // Required if DataType is "float"
+    sasFieldName?: string; // Required for regulatory submissions
+    displayFormat?: string;
+    commentOid?: string;
+    description?: TranslatedText[];
+    codeListRef?: string;
+    origin?: Origin;
+    valueListRef?: string;
 }
 
 export interface ItemRef {
-  itemOid: string;
-  mandatory: 'Yes' | 'No';
-  orderNumber?: number;
-  keySequence?: number; // Required for regulatory submissions
-  methodOid?: string; // Conditional: required when referenced ItemDef has Origin@Type="Derived"
-  role?: string; // Optional for SDTM standard domains, conditional required for SDTM custom domains
-  roleCodeListOid?: string;
-  whereClauseRefs?: string[];
+    itemOid: string;
+    mandatory: "Yes" | "No";
+    orderNumber?: number;
+    keySequence?: number; // Required for regulatory submissions
+    methodOid?: string; // Conditional: required when referenced ItemDef has Origin@Type="Derived"
+    role?: string; // Optional for SDTM standard domains, conditional required for SDTM custom domains
+    roleCodeListOid?: string;
+    whereClauseRefs?: string[];
 }
