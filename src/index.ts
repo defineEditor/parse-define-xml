3ec98c263f42dc634800e4d9023f8c453375fff5
Dmitry <dmitry.m.kolosov@gmail.com>
2025-08-12 14:07:59 -0300

    Preparing for release

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// Main parser function
+export * from "parser/define";
+// Re-export interfaces
+export * from "interfaces/define.xml.core.d";
+export * from "interfaces/define.xml.21.d";
+export * from "interfaces/arm.10.d";
