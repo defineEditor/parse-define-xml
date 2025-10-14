8041d37e1dab0eff84d32413a9c2a079b11636bb
Dmitry <dmitry.m.kolosov@gmail.com>
2025-10-13 21:40:28 -0300

    Updates for export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 // Main parser function
 export * from "./parser/define";
 
-// Export interfaces
-export * from "./interfaces/define.xml";
+// Re-export interfaces
+export * from "./interfaces/define.xml.core";
+export * from "./interfaces/define.xml.20";
+export * from "./interfaces/define.xml.21";
+export * from "./interfaces/arm.10";
